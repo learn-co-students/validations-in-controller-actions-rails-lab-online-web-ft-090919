cc1f3d7b0d8261e49ea88a0e9406e00175aee53d
Dominique Skaf <doskaf@yahoo.com>
2019-11-18 13:56:13 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: { message: "Title can't be blank" }
+    validates :content, length: { minimum: 100, message: "Content too short" }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "Category must be either Fiction or Non-Fiction" }
 end
